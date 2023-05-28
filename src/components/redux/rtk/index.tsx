75d4e5888186c7bc23ac469ac9f158abac9fc559
huzixuan <huzixuan@huzixuan.com>
2023-05-28 17:16:38 +0800

    1. redux rtk 请求数据

--- src/components/redux/rtk/index.tsx
+++ src/components/redux/rtk/index.tsx
@@ -4,7 +4,7 @@ import {
 } from 'react-redux';
 import { Button } from 'antd';
 import { store } from './Store';
-import { addAge } from './modules/Person';
+import { addAge, requestToStoreAction } from './modules/Person';
 
 const Show = () => {
   const { person } = useSelector((state: any) => state, shallowEqual);
@@ -13,10 +13,14 @@ const Show = () => {
     <Button
       onClick={() => {
         dispatch(addAge(2));
+        // 这个 @ts-ignore 待解决
+        // @ts-ignore
+        dispatch(requestToStoreAction({ key: 'a' })); // 这里可以传参（可选）
       }}
     >
       ReduxRtk: {person.obj.age}
     </Button>
+
   );
 };
 
--- src/components/redux/rtk/modules/Person.ts
+++ src/components/redux/rtk/modules/Person.ts
@@ -1,4 +1,6 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { request1 } from '../../../axios/service';
+import { Commit } from '../../../axios/service/module/User';
 
 // person slice
 const personSlice = createSlice(
@@ -17,9 +19,52 @@ const personSlice = createSlice(
         state.obj.age += payload;
       },
     },
+    /** 异步请求写法一 */
+    // extraReducers: {
+    //   [requestToStoreAction.pending]() { // 请求中
+    //     console.log('pending');
+    //   },
+    //   [requestToStoreAction.fulfilled](state, action) { // 响应
+    //     const { payload } = action;
+    //     console.log('fulfilled', payload);
+    //     state.obj.age = payload.length;
+    //   },
+    //   [requestToStoreAction.rejected](_state, _action) { // 失败
+    //     console.log('rejected');
+    //   }
+    // },
+
+    /** 异步请求写法二 */
+    // extraReducers: (builder) => {
+    //   builder
+    //     .addCase(requestToStoreAction.pending, () => {
+    //     })
+    //     .addCase(requestToStoreAction.fulfilled, (state, action) => {
+    //       const {payload} = action;
+    //       console.log('fulfilled', payload);
+    //       state.obj.age = payload.length;
+    //     })
+    //     .addCase(requestToStoreAction.rejected, (_state, _action) => {
+    //     })
+    // },
   },
 );
 
 export const { addAge } = personSlice.actions;
+
+export const requestToStoreAction = createAsyncThunk(
+  'request/age',
+  async (info, store) => {
+    const commits = await request1.get<Commit[]>({
+      url: '/repos/javascript-tutorial/en.javascript.info/commits',
+    });
+
+    // 异步请求写法三（直接不用写一或二）
+    const { dispatch } = store;
+    dispatch(addAge(commits.length));
+
+    return commits;
+  },
+);
 
 export default personSlice.reducer;
