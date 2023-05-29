87514669dec83b5ccbdd0c8af9849209f7b0a4a3
huzixuan <huzixuan@huzixuan.com>
2023-05-29 22:22:14 +0800

    1. react-color

---
+++ src/components/react/ReactColor.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { RGBColor, SketchPicker } from 'react-color';
+import { Popover } from 'antd';
+
+export const ReactColor = () => {
+  const [color, setColor] = useState<RGBColor>({
+    r: 241, g: 112, b: 19, a: 1,
+  });
+
+  const handleChange = (newColor) => {
+    setColor(newColor.rgb);
+  };
+
+  return (
+    <Popover
+      style={{ display: 'inline-block' }}
+      trigger={['click']}
+      content={(
+        <SketchPicker
+          className="picker"
+          color={color}
+          onChange={handleChange}
+        />
+      )}
+    >
+      <div style={{ height: '20px', width: '50px', backgroundColor: `rgba(${color.r}, ${color.g}, ${color.b}, ${color.a})` }} />
+    </Popover>
+
+  );
+};
--- src/components/react/index.tsx
+++ src/components/react/index.tsx
@@ -4,9 +4,17 @@ import { ClassComponent } from './ClassComponent';
 import { ReactTransitionGroup } from './ReactTransitionGroup';
 import CssInJs from './CssInJs';
 import { I18nextReact } from './i18next-react';
+import { ReactColor } from './ReactColor';
 
 export const ReactComp = () => (
   <>
+    <ReactColor />
+    <ReactColor />
+    <ReactColor />
+    <ReactColor />
+    <ReactColor />
+    <ReactColor />
+    <ReactColor />
     <I18nextReact />
     <AddChildren />
     <ClassComponent />
