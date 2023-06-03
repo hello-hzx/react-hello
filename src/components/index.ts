5349c8dd6c5f917ec5f31a33645991795b48aa95
huzixuan <huzixuan@huzixuan.com>
2023-06-03 21:41:31 +0800

    1. 修复webpack打包错误：Minified React error #130; visit https://reactjs.org/docs/error-decoder.html?invariant=130&args[]=undefined&args[]=

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,7 @@
 import React from 'react';
 
 let module = {};
-if (import.meta.webpackHot) {
+if (!Object.keys(import.meta).length) {
   /** 禁止在需要动态导出的文件中使用默认导出（default export） */
   const requireModule = require.context('./', true, /\.tsx$/);
   requireModule.keys().forEach((fileName) => {
